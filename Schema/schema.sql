fe5c5faf760af07aa88ef90ae5fece20574aa9e2
Takahiro Hirakawa <jonhirak@yahoo.com>
2021-11-18 12:25:59 -0800

    Create csv file. Change column names in schema to match data in csv files. Remove recommended and ratings tables in schema.sql.

--- Schema/schema.sql
+++ Schema/schema.sql
@@ -9,33 +9,10 @@ CREATE TABLE products (
   PRIMARY KEY (id)
 );
 
-CREATE TABLE ratings (
-  id INTEGER NOT NULL AUTO_INCREMENT,
-  product_id INTEGER NOT NULL,
-  `1` SMALLINT DEFAULT 0,
-  `2` SMALLINT DEFAULT 0,
-  `3` SMALLINT DEFAULT 0,
-  `4` SMALLINT DEFAULT 0,
-  `5` SMALLINT DEFAULT 0,
-  PRIMARY KEY (id),
-  FOREIGN KEY (product_id)
-    REFERENCES products(id)
-);
-
-CREATE TABLE recommended (
-  id INTEGER NOT NULL AUTO_INCREMENT,
-  product_id INTEGER NOT NULL,
-  `true` SMALLINT DEFAULT 0,
-  `false` SMALLINT DEFAULT 0,
-  PRIMARY KEY (id),
-  FOREIGN KEY (product_id)
-    REFERENCES products(id)
-);
-
 CREATE TABLE characteristics (
   id INTEGER NOT NULL AUTO_INCREMENT,
   product_id INTEGER NOT NULL,
-  type VARCHAR(10) NOT NULL,
+  name VARCHAR(10) NOT NULL,
   value DECIMAL(10, 5) NOT NULL,
   PRIMARY KEY (id),
   FOREIGN KEY (product_id)
@@ -46,15 +23,15 @@ CREATE TABLE reviews (
   id INTEGER NOT NULL AUTO_INCREMENT,
   product_id INTEGER NOT NULL,
   date DATETIME DEFAULT CURRENT_TIMESTAMP,
-  rating TINYINT NOT NULL,
-  recommend BOOLEAN NOT NULL,
   summary VARCHAR(50) NOT NULL,
   body VARCHAR(150) NOT NULL,
-  reviewer_name VARCHAR(20) NOT NULL,
-  response VARCHAR(150),
-  email VARCHAR(30) NOT NULL,
-  helpfulness SMALLINT DEFAULT 0,
+  recommend BOOLEAN NOT NULL,
   reported BOOLEAN DEFAULT false,
+  reviewer_name VARCHAR(20) NOT NULL,
+  reviewer_email VARCHAR(30) NOT NULL,
+  rating TINYINT NOT NULL,
+  response VARCHAR(150),
+  helpfulness SMALLINT DEFAULT 0,
   PRIMARY KEY (id),
   FOREIGN KEY (product_id)
     REFERENCES products(id)
@@ -68,3 +45,26 @@ CREATE TABLE photos (
   FOREIGN KEY (review_id)
     REFERENCES reviews(id)
 );
+
+-- CREATE TABLE ratings (
+--   id INTEGER NOT NULL AUTO_INCREMENT,
+--   product_id INTEGER NOT NULL,
+--   `1` SMALLINT DEFAULT 0,
+--   `2` SMALLINT DEFAULT 0,
+--   `3` SMALLINT DEFAULT 0,
+--   `4` SMALLINT DEFAULT 0,
+--   `5` SMALLINT DEFAULT 0,
+--   PRIMARY KEY (id),
+--   FOREIGN KEY (product_id)
+--     REFERENCES products(id)
+-- );
+
+-- CREATE TABLE recommended (
+--   id INTEGER NOT NULL AUTO_INCREMENT,
+--   product_id INTEGER NOT NULL,
+--   `true` SMALLINT DEFAULT 0,
+--   `false` SMALLINT DEFAULT 0,
+--   PRIMARY KEY (id),
+--   FOREIGN KEY (product_id)
+--     REFERENCES products(id)
+-- );
